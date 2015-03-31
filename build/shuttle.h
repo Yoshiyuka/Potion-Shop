6ba9a1eb760ef294a357465f3ae8e2af814576f6
Yoshiyuka <yoshiyuka@users.noreply.github.com>
2015-03-30 21:16:40 -0700

    Added assets and built headers so this can be compiled on other systems.

---
+++ build/shuttle.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(shuttle)
+
+//======================================================================
+//
+//	shuttle, 64x64@4, 
+//	+ palette 16 entries, not compressed
+//	+ bitmap not compressed
+//	Total size: 32 + 2048 = 2080
+//
+//	Time-stamp: 2014-08-08, 17:52:02
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.12
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SHUTTLE_H
+#define GRIT_SHUTTLE_H
+
+#define shuttleBitmapLen 2048
+extern const unsigned int shuttleBitmap[512];
+
+#define shuttlePalLen 32
+extern const unsigned short shuttlePal[16];
+
+#endif // GRIT_SHUTTLE_H
+
+//}}BLOCK(shuttle)
